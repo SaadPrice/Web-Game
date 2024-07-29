3c30a618f6df4707c757266748eb725d5558fad4
Saad <educationalmindz@gmail.com>
2024-07-29 15:04:57 -0400

    late assignment.

--- index.js
+++ index.js
@@ -1,30 +1,17 @@
+// Create the playable character
+const pc = newPlayableCharacter(100, 110)
+
+// Create a non-playable character
+const npc = newNonPlayableCharacter(50, 300)
+
+// have the NPC start walking east immediately
+npc.walkEast()
+
+// Create the inventory
 const inventory = newInventory()
 move(inventory).to(0, 0)
 
-const character = newImage('assets/green-character/static.gif')
-
-function handleDirectionChange(direction){
-    if(direction === null){
-        character.src = `assets/green-character/static.gif`
-    }
-    if(direction === 'west'){
-        character.src = `assets/green-character/west.gif`
-    }
-    if(direction === 'north'){
-        character.src = `assets/green-character/north.gif`
-    }
-    if(direction === 'east'){
-        character.src = `assets/green-character/east.gif`
-    }
-    if(direction === 'south'){
-        character.src = `assets/green-character/south.gif`
-    }
-}
-
-move(character).withArrowKeys(100, 250, handleDirectionChange)
-
-
-
+// Create everything else
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
